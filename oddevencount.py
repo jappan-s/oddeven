31d2e25fed95bb77ec0f349ac4cfaec0703b57ee
jappan-s <63873951+jappan-s@users.noreply.github.com>
2022-10-16 20:12:06 +0530

    Add files via upload

---
+++ oddevencount.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[7]:
+
+
+numbers=(1,2,3,4,5,6,7,8,9)
+count_even=0
+count_odd=0
+for i in numbers:
+    if i%2==0:
+        count_even+=1
+    else:
+        count_odd+=1
+print("The number of even numbers are :",count_even)
+print("The number of odd numbers are :",count_odd)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
